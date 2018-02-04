7e1d6d593776cad71b0d45e4f3f7bf1c17e6343e
kuvichka maksim <kuvichkamaksim@gmail.com>
2018-02-04 04:00:57 +0200

    implement teachers table

---
+++ src/components/Row.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import injectSheet from 'react-jss';
+
+const styleRow = {
+  row1: {
+    height: '20vh',
+    display: 'flex',
+    flexDirection: 'row',
+    '& > div': {
+      width: '23.5vw',
+      fontSize: '3vh',
+      textAlign: 'center',
+      backgroundColor: '#93A1B6',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  },
+  row2: {
+    height: '20vh',
+    display: 'flex',
+    flexDirection: 'row',
+    '& > div': {
+      width: '23.5vw',
+      fontSize: '3vh',
+      textAlign: 'center',
+      backgroundColor: '#B1BBCC',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  },
+  faces: {
+    height: '18vh',
+  },
+  fullName: {
+    width: '23.5vw',
+  },
+  lesson: {
+    width: '23.5vw',
+  },
+  image: {
+    padding: '1vh 0',
+    display: 'flex',
+    justifyContent: 'center',
+  }
+};
+
+class Row extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { classes } = this.props;
+    let row = {};
+    if (this.props.index % 2 === 0) {
+      row = classes.row1;
+    } else { row = classes.row2};
+    return (
+      <div className={row}>
+        <div className={classes.fullName}><span>{this.props.name}</span></div>
+        <div className={classes.image}>
+          <img className={classes.faces} src={this.props.photo} />
+        </div>
+        <div className={classes.lesson}><span>{this.props.subject}</span></div>
+        <div><span>{this.props.contacts}</span></div>
+      </div>
+    )
+  }
+}
+
+export default injectSheet(styleRow)(Row);
