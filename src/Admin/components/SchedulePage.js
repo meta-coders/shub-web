b622e0089e2bac48ca0f8089c6c360367478d3fc
kuvichka maksim <kuvichkamaksim@gmail.com>
2018-02-04 11:50:48 +0200

    Pre-final commit

--- src/Admin/components/SchedulePage.js
+++ src/Admin/components/SchedulePage.js
@@ -6,10 +6,12 @@ class SchedulePage extends React.Component {
   constructor(props) {
     super(props);
     this.state ={
-      file:null
+      file:null,
+      className: '',
     }
     this.onFormSubmit = this.onFormSubmit.bind(this)
     this.onChange = this.onChange.bind(this)
+    this.onInputChange = this.onInputChange.bind(this)
     this.fileUpload = this.fileUpload.bind(this)
   }
   onFormSubmit(e){
@@ -21,8 +23,11 @@ class SchedulePage extends React.Component {
   onChange(e) {
     this.setState({file:e.target.files[0]})
   }
+  onInputChange(e) {
+    this.setState({className: e.target.value});
+  }
   fileUpload(file){
-    const url = 'http://10.1.49.131:3000/admin/uploadTimetable';
+    const url = 'http://10.1.49.131:3000/admin/uploadSchedule';
     const formData = new FormData();
     formData.append('sessionId', 'ee2193db5839bdd821f25caf173a5f07');
     formData.append('table',file);
@@ -32,15 +37,18 @@ class SchedulePage extends React.Component {
         }
     }
     return post(url, formData, config).then(function(response){
-      console.log(response.data); // ex.: { user: 'Your User'}
-      console.log(response.status); // ex.: 200
+      alert(response.data.length1); // ex.: { user: 'Your User'}
     });
   }
 
   render() {
     return (
       <form onSubmit={this.onFormSubmit}>
-        <h1>File Upload</h1>
+        <h1>Schedule Upload</h1>
+        <span>Class name: </span>
+        <input type="text" value={this.state.className} onChange={this.onInputChange} />
+        <br />
+        <br />
         <input type="file" onChange={this.onChange} />
         <button type="submit">Upload</button>
       </form>
